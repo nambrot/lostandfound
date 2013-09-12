23755c9d4ec857e6f5bd8632c47f3330a5665d21
Nam Chu Hoai <nambrot@googlemail.com>
2013-09-12 13:44:53 -0400

    add mobile experience view paths

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,46 +4,37 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_filter :set_mobile_preferences
-  before_filter :redirect_to_mobile_if_applicable
-  before_filter :prepend_view_path_if_mobile
+  # Uncomment if you want a seperate mobile experience
+  # before_filter :prepend_view_path_if_mobile
 
   private
 
   def set_mobile_preferences
     if params[:mobile_site]
       cookies.delete(:prefer_full_site)
+      cookies.permanent[:prefer_mobile_site] = 1
     elsif params[:full_site]
       cookies.permanent[:prefer_full_site] = 1
-      redirect_to_full_site if mobile_request?
+      cookies.delete(:prefer_mobile_site)
     end
   end
 
   def prepend_view_path_if_mobile
-    if mobile_request?
+    if mobile_request?      
       prepend_view_path Rails.root + 'app' + 'mobile_views'
     end
   end
 
-  def redirect_to_full_site
-    redirect_to request.protocol + request.host_with_port.gsub(/^m\./, '') +
-                request.request_uri and return
-  end
-
-  def redirect_to_mobile_if_applicable
-    unless mobile_request? || cookies[:prefer_full_site] || !mobile_browser?
-      redirect_to request.protocol + "m." + request.host_with_port.gsub(/^www\./, '') +
-                  request.request_uri and return
-    end
-  end
-
   def mobile_request?
-    request.subdomains.first == 'm'
+    cookies[:prefer_mobile_site] and not cookies[:prefer_full_site]
   end
   helper_method :mobile_request?
 
+
   def mobile_browser?
-    request.env["HTTP_USER_AGENT"] && request.env["HTTP_USER_AGENT"][/(iPhone|iPod|iPad|Android)/]
+    # enable if you want to detect mobile user agent
+    # request.env["HTTP_USER_AGENT"] && request.env["HTTP_USER_AGENT"][/(iPhone|iPod|iPad|Android)/]
   end
   helper_method :mobile_browser?
-  
+
 end
